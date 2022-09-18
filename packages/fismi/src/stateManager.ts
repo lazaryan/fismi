ec01821897fb6819bc5da38021c1ebd3a5aabd7b
lazaryan <lazaryan99@gmail.com>
2022-09-19 00:11:51 +0300

    add update subscribe for async tokens

--- packages/fismi/src/stateManager.ts
+++ packages/fismi/src/stateManager.ts
@@ -4,15 +4,59 @@ import type {
 
 import type { ControllerToken } from './controller';
 
-export type SubscriptionTokenAction<T> = ((value: Pick<TokenState<T>, 'isActive'>) => void);
-export type SubscriptionControllerAction<T> = ((value: ControllerState<T>['value']) => void);
+import { isPromise } from './utils';
 
-type ControllerState<T> = {
+export type ReturnSubscriptionControllerDataIsDisactive<T> = {
+  isActive: false;
+  value?: T;
+}
+
+export type ReturnSubscriptionControllerDataIsActive<T> = {
+  isActive: true;
+  value?: T;
+}
+
+export type ReturnSubscriptionControllerData<T> = ReturnSubscriptionControllerDataIsActive<T> | ReturnSubscriptionControllerDataIsDisactive<T>;
+
+export type SubscriptionTokenAction<T> = ((value: Pick<TokenState<T>, 'isActive'>) => void);
+export type SubscriptionControllerAction<T> = ((value: ReturnSubscriptionControllerData<T>) => void);
+
+export type ControllerLoadStatus = 'init' | 'process' | 'fail' | 'done';
+export type AsyncControllerValue<T> = () => T | Promise<T>;
+
+type ControllerStateSync<T> = {
   value: T;
   defaultValue?: T;
-  isLoad: boolean;
-  subscriptions: Set<any>;
-};
+  isAsync: false;
+  processStatus: 'init';
+  subscriptions: Set<SubscriptionControllerAction<T>>;
+}
+
+type ControllerStateAsyncIsFail<T> = {
+  value?: any;
+  defaultValue?: T;
+  isAsync: true;
+  processStatus: 'fail';
+  subscriptions: Set<SubscriptionControllerAction<T>>;
+}
+
+type ControllerStateAsyncIsLoad<T> = {
+  value: T;
+  defaultValue?: T;
+  isAsync: true;
+  processStatus: 'done';
+  subscriptions: Set<SubscriptionControllerAction<T>>;
+}
+
+type ControllerStateAsync<T> = {
+  value: AsyncControllerValue<T>;
+  defaultValue?: T;
+  isAsync: true;
+  processStatus: 'init' | 'process';
+  subscriptions: Set<SubscriptionControllerAction<T>>;
+}
+
+type ControllerState<T> = ControllerStateSync<T> | ControllerStateAsyncIsLoad<T> | ControllerStateAsyncIsFail<T> | ControllerStateAsync<T>;
 
 type TokenState<T> = {
   isActive: boolean;
@@ -65,7 +109,12 @@ class StateManager implements StateManagerI {
     this.state.delete(token.symbol);
   }
 
-  bindControllerValue<T>(token: FeatureToken<T>, controllerToken: ControllerToken, value: T, defaultValue?: T): void {
+  bindControllerValue<T, C extends ControllerState<T>>(
+    token: FeatureToken<T>,
+    controllerToken: ControllerToken,
+    value: C['value'],
+    defaultValue?: T
+  ): void {
     if (!this.state.has(token.symbol)) {
       this.addToken(token);
     }
@@ -75,7 +124,8 @@ class StateManager implements StateManagerI {
       {
         value,
         defaultValue,
-        isLoad: !token.isAsync,
+        isAsync: token.isAsync ?? false,
+        processStatus: 'init',
         subscriptions: new Set(),
       },
     );
@@ -110,22 +160,98 @@ class StateManager implements StateManagerI {
 
     const oldState = this.state.get(token.symbol) as TokenState<T>;
 
-    if (!status) {
-      const newState: TokenState<T> = {
-        ...oldState,
-        isActive: false,
-      };
+    const newState: TokenState<T> = {
+      ...oldState,
+      isActive: status,
+    };
 
-      this.state.set(token.symbol, newState);
+    this.state.set(token.symbol, newState);
 
-      oldState.subscribes.forEach((callback) => {
-        callback({
-          isActive: oldState.isActive,
-        })
+    oldState.subscribes.forEach((callback) => {
+      callback({
+        isActive: status,
       })
+    });
+  
+    oldState.controllerState.forEach((controllerState, tokenSymbol) => {
+      if(!controllerState.isAsync) {
+        controllerState.subscriptions.forEach((callback) => {
+          const returnValue: ReturnSubscriptionControllerData<T> = !status
+            ? { isActive: false, value: controllerState.defaultValue }
+            : { isActive: true, value: controllerState.value };
 
-      return;
-    }
+            callback(returnValue);
+        });
+
+        return;
+      }
+  
+      if (controllerState.processStatus === 'done') {
+        controllerState.subscriptions.forEach((callback) => {
+          const returnValue: ReturnSubscriptionControllerData<T> = !status
+            ? { isActive: false, value: controllerState.defaultValue }
+            : { isActive: true, value: controllerState.value };
+
+            callback(returnValue);
+        });
+
+        return;
+      }
+
+      if (controllerState.processStatus === 'init') {
+        if (!status) {
+          controllerState.subscriptions.forEach((callback) => {
+            callback({ isActive: false, value: controllerState.defaultValue });
+          });
+        } else {
+          const value = controllerState.value();
+
+          const oldControllerState = oldState.controllerState.get(tokenSymbol) as Exclude<ReturnType<typeof oldState.controllerState.get>, undefined>;
+
+          if (isPromise(value)) {
+            oldControllerState.subscriptions.forEach((callback) => {
+              callback({ isActive: true, value: oldControllerState.defaultValue });
+            });
+
+            (value as Promise<T>)
+              .then((response) => {
+                oldState.controllerState.set(tokenSymbol, {
+                  ...oldControllerState,
+                  processStatus: 'done',
+                  value: response,
+                } as ControllerStateAsyncIsLoad<any>); // TODO
+
+                const activeStatus = this.state.get(token.symbol)?.isActive ?? false;
+
+                oldControllerState.subscriptions.forEach((callback) => {
+                  callback({ isActive: activeStatus, value: !activeStatus ? undefined : response });
+                });
+              })
+              .catch(() => {
+                oldState.controllerState.set(tokenSymbol, {
+                  ...oldControllerState,
+                  processStatus: 'fail',
+                  value: undefined,
+                } as ControllerStateAsyncIsFail<any>); // TODO
+
+                oldControllerState.subscriptions.forEach((callback) => {
+                  callback({ isActive: true, value: undefined });
+                });
+              })
+          } else {
+            oldState.controllerState.set(tokenSymbol, {
+              ...oldControllerState,
+              processStatus: 'done',
+              value,
+            } as ControllerStateAsyncIsLoad<any>); // TODO
+
+            oldControllerState.subscriptions.forEach((callback) => {
+              callback({ isActive: true, value: value as any}); // TODO
+            });
+          }
+        }
+      }
+    })
   }
 
   clear(): void {
