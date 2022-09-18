3b9978532863a2081604a19945ac6f5f27a24f67
lazaryan <lazaryan99@gmail.com>
2022-09-19 02:24:36 +0300

    fix builder

---
+++ packages/fismi/vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'fismi',
+      fileName: 'build'
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+});
